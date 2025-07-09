a917ce3e130bfa8c4d36cb06fb2ab623f25705ac
Lillie <lbrugioni@luc.edu>
2025-07-09 21:55:57 +0000

    Final commit

--- week06.py
+++ week06.py
@@ -1,13 +1,13 @@
-#This fucntion reads temperature values from a file
-#and returns them as a lsit of floats
+#Reads temperature values from specifced file and returns a list of floats.
 def load_to_list(filepath:str)->list[float]:
     numbers=[]
     with open(filepath,'r') as file:
         for line in file:
-            value=float(line.strip())
+            value=float(line.strip()) #Convert each line to float and then add to the list.
             numbers.append(value)
     return numbers
-#This fucntion prints basic statistics or a list of numbers
+
+#This function calculates and prints count,average,min and max of temperature list.
 def descriptive_statistics(source_data:list[float])->None:
     count=len(source_data)
     total=sum(source_data)
@@ -19,8 +19,9 @@ def descriptive_statistics(source_data:list[float])->None:
     print(f"The average value is {average}")
     print(f"The highest value is {largest} and the smallest value is {smallest}.")
 
-#This function reads lines from a file and applies
-#simple markup rules for formatting text
+#This function reads lines from a file and applies simple markup rules for formatting text.
+#Words starting with '.' are printed in uppercase.
+#Words starting with '_' are expanded by adding spaces between the letters. 
 def apply_markup(filepath:str)->None:
     with open(filepath,'r') as file:
         for line in file:
@@ -29,11 +30,9 @@ def apply_markup(filepath:str)->None:
 
             for word in words:
                 if word.startswith('.'):
-                    #remove the dot and make uppercase
-                    result.append(word[1:].upper())
-                elif word.startswith('_'):
-                    #remove the underscore and add spaces between letters 
-                    expanded=''.join(word[1:])
+                    result.append(word[1:].upper()) #Remove the dot and make uppercase.
+                elif word.startswith('_'): #Remove the underscore and add spaces between letters.
+                    expanded=' '.join(word[1:])
                     result.append(expanded)
                 else:
                     result.append(word)
